efc31261f982ab648af28640e316485027959ac4
Luiz Fernando Elias de Mello Júnior <luizfernando215@gmail.com>
2023-11-26 22:27:08 -0300

    chore: quiz MMA

--- lib/models/layout_questions_model.dart
+++ lib/models/layout_questions_model.dart
@@ -23,8 +23,7 @@ class LayOutQuestion {
       text: text,
       options: options
           .map(
-            (option) =>
-                LayOutOption(text: option.text, isCorrect: option.isCorrect),
+            (option) => LayOutOption(text: option.text, isCorrect: option.isCorrect),
           )
           .toList(),
       isLocked: isLocked,
@@ -46,104 +45,114 @@ class LayOutOption {
 
 final layOutQuestionsList = [
   LayOutQuestion(
-    text: "I control how widgets are placed vertically in a column. Who am I?",
+    text: 'Quem é conhecido como "The Notorious" no mundo do MMA?',
     options: [
-      const LayOutOption(text: "MainAxisAlignment", isCorrect: true),
-      const LayOutOption(text: "Row", isCorrect: false),
-      const LayOutOption(text: "CrossAxisAlignment", isCorrect: false),
-      const LayOutOption(text: "mainAxisSize", isCorrect: false),
+      const LayOutOption(text: "Jon Jones", isCorrect: false),
+      const LayOutOption(text: "Georges St-Pierre", isCorrect: false),
+      const LayOutOption(text: "Khabib Nurmagomedov", isCorrect: false),
+      const LayOutOption(text: "Conor McGregor", isCorrect: true),
     ],
     id: 0,
-    correctAnswer:
-        const LayOutOption(text: "MainAxisAlignment", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "Conor McGregor", isCorrect: true),
   ),
   LayOutQuestion(
-    text:
-        "I allow widgets to expand and contract based on available space. You'll always find me inside a Flex. Who am I?",
+    text: "Qual é a organização de MMA mais famosa e influente do mundo?",
     options: [
-      const LayOutOption(text: "Flexible ", isCorrect: true),
-      const LayOutOption(text: "Expanded", isCorrect: false),
-      const LayOutOption(text: "Flex", isCorrect: false),
-      const LayOutOption(text: "mainAxisSpacing", isCorrect: false),
+      const LayOutOption(text: "Bellator", isCorrect: false),
+      const LayOutOption(text: "ONE Championship", isCorrect: false),
+      const LayOutOption(text: "UFC", isCorrect: true),
+      const LayOutOption(text: "PFL", isCorrect: false),
     ],
     id: 1,
-    correctAnswer: const LayOutOption(text: "Flexible", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "UFC", isCorrect: true),
   ),
-
   LayOutQuestion(
-    text:
-        "I create an invisible bounding box that controls my child's width and height. What am I?",
+    text: 'Quem é considerado o "Rei da Selva" no MMA brasileiro?',
     options: [
-      const LayOutOption(text: "Container ", isCorrect: true),
-      const LayOutOption(text: "SizedBox", isCorrect: false),
-      const LayOutOption(text: "Card", isCorrect: false),
-      const LayOutOption(text: "Row", isCorrect: false),
+      const LayOutOption(text: "José Aldo", isCorrect: false),
+      const LayOutOption(text: "Anderson Silva", isCorrect: false),
+      const LayOutOption(text: "Wanderlei Silva", isCorrect: true),
+      const LayOutOption(text: "Fabricio Werdum", isCorrect: false),
     ],
     id: 2,
-    correctAnswer: const LayOutOption(text: "Container", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "Wanderlei Silva", isCorrect: true),
   ),
-
   LayOutQuestion(
-    text:
-        "I align my children widgets to the start or end of the row. Who am I?",
+    text: "Qual é a técnica de finalização em que um lutador utiliza suas pernas para estrangular o oponente?",
     options: [
-      const LayOutOption(text: "SingleChildScrollView", isCorrect: false),
-      const LayOutOption(text: "crossAxisCount", isCorrect: false),
-      const LayOutOption(text: "MainAxisAlignment ", isCorrect: true),
-      const LayOutOption(text: "crossAxisSpacing", isCorrect: false),
+      const LayOutOption(text: "Armlock", isCorrect: false),
+      const LayOutOption(text: "Kimura", isCorrect: false),
+      const LayOutOption(text: "Triângulo", isCorrect: true),
+      const LayOutOption(text: "RNC (Rear-Naked Choke)", isCorrect: false),
     ],
     id: 3,
-    correctAnswer:
-        const LayOutOption(text: "MainAxisAlignment ", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "Triângulo", isCorrect: true),
   ),
-  // other 4
   LayOutQuestion(
-    text:
-        "I'm a widget that lets you precisely position children using x, y coordinates. Who am I?",
+    text: 'Quem é conhecido por seu apelido "The Last Stylebender"?',
     options: [
-      const LayOutOption(text: "Align", isCorrect: false),
-      const LayOutOption(text: "FittedBox", isCorrect: false),
-      const LayOutOption(text: "Postioned", isCorrect: false),
-      const LayOutOption(text: "Stack ", isCorrect: true),
+      const LayOutOption(text: "Israel Adesanya", isCorrect: true),
+      const LayOutOption(text: "Robert Whittaker", isCorrect: false),
+      const LayOutOption(text: "Kelvin Gastelum", isCorrect: false),
+      const LayOutOption(text: "Paulo Costa", isCorrect: false),
     ],
     id: 4,
-    correctAnswer: const LayOutOption(text: "Stack ", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "Israel Adesanya", isCorrect: true),
   ),
   LayOutQuestion(
-    text: "I'm a horizontal version of Column. Who am I?",
+    text: 'Qual lutador é famoso por sua técnica "Showtime Kick"?',
     options: [
-      const LayOutOption(text: "Row ", isCorrect: true),
-      const LayOutOption(text: "Divider", isCorrect: false),
-      const LayOutOption(text: "Column", isCorrect: false),
-      const LayOutOption(text: "Stack", isCorrect: false),
+      const LayOutOption(text: "Nate Diaz", isCorrect: false),
+      const LayOutOption(text: "Tony Ferguson", isCorrect: false),
+      const LayOutOption(text: "Anthony Pettis", isCorrect: true),
+      const LayOutOption(text: "Donald Cerrone", isCorrect: false),
     ],
     id: 5,
-    correctAnswer: const LayOutOption(text: "Row", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "Anthony Pettis", isCorrect: true),
   ),
-
   LayOutQuestion(
-    text:
-        "I align widgets to the top, bottom, center inside a Column. What am I?",
+    text: "Quem foi o primeiro campeão peso-pesado do UFC?",
     options: [
-      const LayOutOption(text: "Row", isCorrect: false),
-      const LayOutOption(text: "Align", isCorrect: false),
-      const LayOutOption(text: "Spacer", isCorrect: false),
-      const LayOutOption(text: "MainAxisAlignment ", isCorrect: true),
+      const LayOutOption(text: "Randy Couture", isCorrect: true),
+      const LayOutOption(text: "Brock Lesnar", isCorrect: false),
+      const LayOutOption(text: "Frank Mir", isCorrect: false),
+      const LayOutOption(text: "Mark Coleman", isCorrect: false),
     ],
     id: 6,
-    correctAnswer:
-        const LayOutOption(text: "MainAxisAlignment ", isCorrect: true),
+    correctAnswer: const LayOutOption(text: "Randy Couture", isCorrect: true),
+  ),
+  LayOutQuestion(
+    text: "Qual é o lutador russo invicto que se aposentou com um recorde de 29-0?",
+    options: [
+      const LayOutOption(text: "Fedor Emelianenko", isCorrect: false),
+      const LayOutOption(text: "Khabib Nurmagomedov", isCorrect: true),
+      const LayOutOption(text: "Alexander Gustafsson", isCorrect: false),
+      const LayOutOption(text: "Stipe Miocic", isCorrect: false),
+    ],
+    id: 7,
+    correctAnswer: const LayOutOption(text: "Khabib Nurmagomedov", isCorrect: true),
+  ),
+  LayOutQuestion(
+    text: "Quem detém o recorde de defesas consecutivas do cinturão dos meio-médios no UFC?",
+    options: [
+      const LayOutOption(text: "Georges St-Pierre", isCorrect: false),
+      const LayOutOption(text: "yron Woodley", isCorrect: false),
+      const LayOutOption(text: "Matt Hughes", isCorrect: true),
+      const LayOutOption(text: "Johny Hendricks", isCorrect: false),
+    ],
+    id: 8,
+    correctAnswer: const LayOutOption(text: "Matt Hughes", isCorrect: true),
   ),
   LayOutQuestion(
     text:
-        "I align my Row or Column children differently based on available space. Who am I?",
+        'Qual é o evento de MMA japonês conhecido por suas regras únicas, como chutes na cabeça de um oponente caído?',
     options: [
-      const LayOutOption(text: "Expanded", isCorrect: false),
-      const LayOutOption(text: "Flex ", isCorrect: true),
-      const LayOutOption(text: "FittedBox", isCorrect: false),
-      const LayOutOption(text: "Wrap", isCorrect: false),
+      const LayOutOption(text: "Pancrase", isCorrect: false),
+      const LayOutOption(text: "Rizin FF", isCorrect: true),
+      const LayOutOption(text: "Dream", isCorrect: false),
+      const LayOutOption(text: "Shooto", isCorrect: false),
     ],
-    id: 7,
-    correctAnswer: const LayOutOption(text: "Scoped Model", isCorrect: true),
+    id: 9,
+    correctAnswer: const LayOutOption(text: "Rizin FF", isCorrect: true),
   ),
 ];
